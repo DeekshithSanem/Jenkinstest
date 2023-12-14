799f4fd5e1e5e3a65bb3c52c3961129fabae7251
DeekshithSanem <47519589+DeekshithSanem@users.noreply.github.com>
2023-12-14 12:01:08 +0530

    Create two.py

---
+++ two.py
@@ -0,0 +1,15 @@
+import sys
+ 
+if len(sys.argv) == 3 and sys.argv[1] == '-f' and sys.argv[2] == 'abc.csv':
+    # Your code for processing the provided filename goes here
+    print("Processing abc.csv file...")
+    # Example: Read the contents of abc.csv
+    try:
+        with open('abc.csv', 'r') as file:
+            data = file.read()
+            print(data)  # Just an example, you can perform any processing here
+    except FileNotFoundError:
+        print("File not found.")
+else:
+    print("Usage: python filename.py -f abc.csv")
+#has context menuComposeParagraph
